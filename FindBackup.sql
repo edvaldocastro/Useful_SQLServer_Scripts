961b6be0a7e3d0a43f10869b936aae1d42aea08b
Edvaldo Castro <edvaldo.castro@outlook.com>
2017-07-12 15:52:55 +0100

    Update FindBackup.sql

--- FindBackup.sql
+++ FindBackup.sql
@@ -1,13 +1,12 @@
 --search for backups
-declare @databaseName varchar(200) = 'msdb'
-declare @type char(1) = 'D'
-declare @startSearchingDate datetime = '2016-04-01 00:00:00.000'
-declare @FinishSearchingDate datetime = '2016-05-10 23:59:59.665'
-
-select bs.database_name, bs.type, bs.backup_finish_date,bs.backup_size, bmf.physical_device_name
+declare @startSearchingDate datetime = getdate() - 30
+declare @FinishSearchingDate datetime = getdate() 
+select bs.database_name, bs.type, bs.backup_finish_date,bmf.physical_device_name,bs.backup_size,* 
   from msdb..backupset bs
   join msdb..backupmediafamily bmf
     on bs.media_set_id = bmf.media_set_id
-where database_name = @databaseName
-   and bs.type = @type
+ where 1=1 
+   --and database_name = 'master'
+   and bs.type in ('D','I','L')
    and bs.backup_finish_date between @startSearchingDate and @FinishSearchingDate
+ order by bs.backup_finish_date desc
