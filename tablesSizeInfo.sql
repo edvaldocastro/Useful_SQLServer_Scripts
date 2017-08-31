cdc026943267d6ac7e3f50e8f3b67c3e3e83fa5a
Edvaldo Castro <edvaldo.castro@outlook.com>
2017-08-31 17:03:33 +0100

    Update tablesSizeInfo.sql

--- tablesSizeInfo.sql
+++ tablesSizeInfo.sql
@@ -1,30 +1,33 @@
+/* 05 */   
  -- Table information
 select s.name as 'SchemaName',
        t.name as 'TableName',
-          CAST(SUM(a.total_pages) * 8 as numeric(12,2)) as 'TotalSpaceKB',
-          CAST(SUM(a.total_pages) * 8 /1024. as numeric(12,2)) as 'TotalSpaceMB',
-          CAST(SUM(a.used_pages) * 8 as numeric(12,2)) as 'UsedSpaceKB',
-          CAST(SUM(a.used_pages) * 8 / 1024. as numeric(12,2)) as 'UsedSpaceMB',
-          (SUM(a.total_pages) - SUM(a.used_pages)) * 8 as 'UnusedSpaceKB',
-          CAST((a.used_pages * 8. / p.rows)as numeric(12,2)) as 'AVGRowSizeKB',
-          case p.partition_number
-                     when 1 then 'Not Partitioned'
-                     else 'Partitioned'
-          end as 'IsTablePartitioned',
-          p.data_compression_desc as 'DataCompressionType'
+	   p.rows,
+	   CAST(SUM(a.total_pages) * 8 as numeric(12,2)) as 'TotalSpaceKB', 
+	   CAST(SUM(a.total_pages) * 8 /1024. as numeric(12,2)) as 'TotalSpaceMB', 
+	   CAST(SUM(a.used_pages) * 8 as numeric(12,2)) as 'UsedSpaceKB', 
+	   CAST(SUM(a.used_pages) * 8 / 1024. as numeric(12,2)) as 'UsedSpaceMB', 
+	   (SUM(a.total_pages) - SUM(a.used_pages)) * 8 as 'UnusedSpaceKB',
+	   CAST((a.used_pages * 8. / p.rows)as numeric(12,2)) as 'AVGRowSizeKB',
+	   case p.partition_number
+			when 1 then 'Not Partitioned'
+			else 'Partitioned'
+	   end as 'IsTablePartitioned', 
+	   p.data_compression_desc as 'DataCompressionType',
+	   'select * from '+s.name+'.'+t.name
   from sys.tables t
-  join sys.indexes i
+  join sys.indexes i 
     on t.OBJECT_ID = i.object_id
-  join sys.partitions p
-    on i.object_id = p.OBJECT_ID
+  join sys.partitions p 
+    on i.object_id = p.OBJECT_ID 
    and i.index_id = p.index_id
-  join sys.allocation_units a
+  join sys.allocation_units a 
     on p.partition_id = a.container_id
-  left join sys.schemas s
+  left join sys.schemas s 
     on t.schema_id = s.schema_id
-where t.name not like 'dt%'
+ where t.name not like 'dt%' 
    and t.is_ms_shipped = 0
-   and i.object_id > 255
+   and i.object_id > 255 
    and p.rows > 0
-group by t.Name, s.Name, p.Rows, a.used_pages,p.partition_number,p.data_compression_desc
-order by 4 desc
+ group by t.Name, s.Name, p.Rows, a.used_pages,p.partition_number,p.data_compression_desc
+ order by a.used_pages desc
