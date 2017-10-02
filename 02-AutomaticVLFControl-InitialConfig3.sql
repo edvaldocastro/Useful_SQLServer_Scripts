cb80ca8bb48d4a875e4216b70653680cc8d13800
Edvaldo Castro <edvaldo.castro@outlook.com>
2017-10-02 20:22:21 +0100

    Rename 02-AutomaticVLFControl-InitialConfig3.txt to 02-AutomaticVLFControl-InitialConfig3.sql

--- 02-AutomaticVLFControl-InitialConfig3.sql
+++ 02-AutomaticVLFControl-InitialConfig3.sql
@@ -1,12 +1,12 @@
 /*
-	Nome: Controle Autom·tico de VLFs  // Automatic VLF Control
+	Nome: Controle Autom√°tico de VLFs  // Automatic VLF Control
 	Autor: Edvaldo Castro
 
 	Data: 05/01/2014  // Date: 2014/01/05
 
 	
-	FunÁ„o: Controlar Dinamicamente o tamanho dos VLFs das bases de dados. Este script especificamente realiza
-			o ajuste inicial, adequando os arquivos de Transaction Log de todas as bases de dados da inst‚ncia.
+	Fun√ß√£o: Controlar Dinamicamente o tamanho dos VLFs das bases de dados. Este script especificamente realiza
+			o ajuste inicial, adequando os arquivos de Transaction Log de todas as bases de dados da inst√¢ncia.
 
 	Function:	Dynamically control the VLF size of all databases in a single SQL Server instance. This script do
 				executes the initial adjustments, setting up the size and VLFs for Transaction Log files.
@@ -14,11 +14,11 @@
 
 	Disclaimer:
 			
-		Os passos e scripts a seguir foram desenvolvidos para automatizar a configuraÁ„o e controle dos Virtual Log Files,
-		e podem causar algum tipo de bloqueio ou indisponibilidade tempor·ria do SQL Server. 
-		Antes de implantar ou utilizar partes ou todo o conte˙do aqui disponibilizado, sugiro a leitura e entendimento do que 
-		cada passo realiza. Implante primeiro em um ambiente de testes controlado, e caso veja benefÌcios, se vocÍ for execut·-los 
-		em seu ambiente de produÁ„o, por sua conta e risco.
+		Os passos e scripts a seguir foram desenvolvidos para automatizar a configura√ß√£o e controle dos Virtual Log Files,
+		e podem causar algum tipo de bloqueio ou indisponibilidade tempor√°ria do SQL Server. 
+		Antes de implantar ou utilizar partes ou todo o conte√∫do aqui disponibilizado, sugiro a leitura e entendimento do que 
+		cada passo realiza. Implante primeiro em um ambiente de testes controlado, e caso veja benef√≠cios, se voc√™ for execut√°-los 
+		em seu ambiente de produ√ß√£o, por sua conta e risco.
 
 	Disclaimer
 
@@ -29,7 +29,7 @@
 		
 		
 		07/01/2015
-			The code now verifies if the database is in a MULTI_USER mode and if it¥s READ_ONLY.
+			The code now verifies if the database is in a MULTI_USER mode and if it¬¥s READ_ONLY.
 		
 
 */
@@ -43,18 +43,18 @@ GO
 CREATE PROCEDURE sp_InitialSetupVLFControl
 AS
 
-			/* Vari·veis  -  Variables*/
+			/* Vari√°veis  -  Variables*/
 			DECLARE @COUNT SMALLINT = 1 -- contador para o loop nas bases de dados // Counter for loop on each database
-			DECLARE @DBNAME VARCHAR(100) -- Nome do banco que estar· sendo verificado // Database Name that is currently been verified
-			DECLARE @LOGNAME VARCHAR(100) -- Nome lÛgico do arquivo de log // Logical Name for the Transaction Log File
-			DECLARE @MSG VARCHAR(100) -- Retorno de conclus„o com sucesso apÛs cada base ser verificada // Sucessful message returned after each operation
-			DECLARE @SQL VARCHAR(MAX) -- ConstruÁ„o din‚mica do comando para adequaÁ„o dos VLFs // Dynamic query for VLFs configuration
+			DECLARE @DBNAME VARCHAR(100) -- Nome do banco que estar√° sendo verificado // Database Name that is currently been verified
+			DECLARE @LOGNAME VARCHAR(100) -- Nome l√≥gico do arquivo de log // Logical Name for the Transaction Log File
+			DECLARE @MSG VARCHAR(100) -- Retorno de conclus√£o com sucesso ap√≥s cada base ser verificada // Sucessful message returned after each operation
+			DECLARE @SQL VARCHAR(MAX) -- Constru√ß√£o din√¢mica do comando para adequa√ß√£o dos VLFs // Dynamic query for VLFs configuration
 			DECLARE @TGT_SIZE_MB INT -- Tamanho final (ideal) do arquivo de log // Final (recommended) size for the Transaction Log File according the ControlaVLF table
-			DECLARE @CUR_SIZE_MB INT = 0  -- Tamanho atual do arquivo de log durante a execuÁ„o do growth // Current size for the T-Log File during the growth execution
-			DECLARE @ITER_SIZE_MB SMALLINT -- Tamanho do crescimento a cada loop (esta vari·vel determina o tamanho de cada VLF individualmente) 
+			DECLARE @CUR_SIZE_MB INT = 0  -- Tamanho atual do arquivo de log durante a execu√ß√£o do growth // Current size for the T-Log File during the growth execution
+			DECLARE @ITER_SIZE_MB SMALLINT -- Tamanho do crescimento a cada loop (esta vari√°vel determina o tamanho de cada VLF individualmente) 
 										   -- File Growth size im MB each loop (this variable determines the size of each VLF)	
 
-			--Executa um loop em todas as bases de dados da inst‚ncia atribuindo as vari·veis
+			--Executa um loop em todas as bases de dados da inst√¢ncia atribuindo as vari√°veis
 			-- Loop in every single database in the SQL Server instance, setting the variable values
 			WHILE (@COUNT <= (SELECT MAX(ID) FROM master..controlaVLF))
 				BEGIN
@@ -62,13 +62,13 @@ AS
 					SELECT @DBNAME = DatabaseName,
 						   @LOGNAME = LogFileName,
 						   @TGT_SIZE_MB = TargetLogFileSize_MB,
-						   @ITER_SIZE_MB = 4096,  -- Defina aqui o tamanho do crescimento a cada iteraÁ„o (2048MB = 128MB/VLF  |  4096MB = 256MB/VLF  | 8192MB = 512MB/VLF)
+						   @ITER_SIZE_MB = 4096,  -- Defina aqui o tamanho do crescimento a cada itera√ß√£o (2048MB = 128MB/VLF  |  4096MB = 256MB/VLF  | 8192MB = 512MB/VLF)
 												  -- Change the size of growth for each iteration (2048MB = 128MB/VLF  |  4096MB = 256MB/VLF  | 8192MB = 512MB/VLF)
 						   @CUR_SIZE_MB = 0,
 
-						   -- Caso o Recovery model seja full, realiza um bakcup de log e em seguida um shrinkfile, caso contr·rio, apenas o shrink file
+						   -- Caso o Recovery model seja full, realiza um bakcup de log e em seguida um shrinkfile, caso contr√°rio, apenas o shrink file
 						   -- Checks the Recovery Model, if Full, a Transaction Log Backup is issued, otherwise, only shrinkfile is issued.
-						   -- Monta a query dinamica com base no modelo de recuperaÁ„o do banco | Substitutua o texto abaixo entre coment·rios /* */ pela sua rotina de backup de log
+						   -- Monta a query dinamica com base no modelo de recupera√ß√£o do banco | Substitutua o texto abaixo entre coment√°rios /* */ pela sua rotina de backup de log
 						   -- Creates the dynamic query based on recovery model of the database | Replace the comments /* */ by your T-Log procedure
 						   @SQL =	CASE RecoveryModelDesc
 										WHEN 'SIMPLE' THEN 'USE ' +@DBNAME+ '; DBCC SHRINKFILE(2,EMPTYFILE); USE ' +@DBNAME+ '; DBCC SHRINKFILE(2,EMPTYFILE);'
@@ -90,7 +90,7 @@ AS
 					PRINT 'VALOR ATUAL: '+CAST(@CUR_SIZE_MB AS VARCHAR(5))
 					PRINT 'VALOR ALVO: '+CAST(@TGT_SIZE_MB AS VARCHAR(5))
 		
-				-- Executa um loop em cada uma das bases incrementando pelo tamanho do @ITER_SIZE_MB atÈ atingir o @TGT_SIZE_MB
+				-- Executa um loop em cada uma das bases incrementando pelo tamanho do @ITER_SIZE_MB at√© atingir o @TGT_SIZE_MB
 				-- Runs a loop on each database increasing the file size by the @ITER_SIZE_MB while  it is smaller than @TGT_SIZE_MB
 				WHILE (@CUR_SIZE_MB < @TGT_SIZE_MB)
 						BEGIN
