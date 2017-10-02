ecb85bef1551b92c1f1cf259899a44d25ed58681
Edvaldo Castro <edvaldo.castro@outlook.com>
2017-10-02 20:22:42 +0100

    Rename 03-AutomaticVLFControl-OnDemandCheck3.txt to 03-AutomaticVLFControl-OnDemandCheck3.sql

--- 03-AutomaticVLFControl-OnDemandCheck3.sql
+++ 03-AutomaticVLFControl-OnDemandCheck3.sql
@@ -1,27 +1,27 @@
 /*
-	Nome: Controle Automático de VLFs  // Automatic VLF Control
+	Nome: Controle AutomÃ¡tico de VLFs  // Automatic VLF Control
 	Autor: Edvaldo Castro
 
 	Data: 05/01/2014  // Date: 2014/01/05
 
 	
-	Função: Controlar Dinamicamente o tamanho dos VLFs das bases de dados. Este script especificamente realiza
-			o ajuste inicial, adequando os arquivos de Transaction Log de todas as bases de dados da instância.
+	FunÃ§Ã£o: Controlar Dinamicamente o tamanho dos VLFs das bases de dados. Este script especificamente realiza
+			o ajuste inicial, adequando os arquivos de Transaction Log de todas as bases de dados da instÃ¢ncia.
 
 	Function:	Dynamically control the VLF size of all databases in a single SQL Server instance. This script do
 				executes the initial adjustments, setting up the size and VLFs for Transaction Log files.
 	
 	07/01/2015	
-	The code now verifies if the database is in a MULTI_USER mode and if it´s READ_ONLY.	
+	The code now verifies if the database is in a MULTI_USER mode and if itÂ´s READ_ONLY.	
 
 
 	Disclaimer:
 			
-		Os passos e scripts a seguir foram desenvolvidos para automatizar a configuração e controle dos Virtual Log Files,
-		e podem causar algum tipo de bloqueio ou indisponibilidade temporária do SQL Server. 
-		Antes de implantar ou utilizar partes ou todo o conteúdo aqui disponibilizado, sugiro a leitura e entendimento do que 
-		cada passo realiza. Implante primeiro em um ambiente de testes controlado, e caso veja benefícios, se você for executá-los 
-		em seu ambiente de produção, por sua conta e risco.
+		Os passos e scripts a seguir foram desenvolvidos para automatizar a configuraÃ§Ã£o e controle dos Virtual Log Files,
+		e podem causar algum tipo de bloqueio ou indisponibilidade temporÃ¡ria do SQL Server. 
+		Antes de implantar ou utilizar partes ou todo o conteÃºdo aqui disponibilizado, sugiro a leitura e entendimento do que 
+		cada passo realiza. Implante primeiro em um ambiente de testes controlado, e caso veja benefÃ­cios, se vocÃª for executÃ¡-los 
+		em seu ambiente de produÃ§Ã£o, por sua conta e risco.
 
 	Disclaimer
 
@@ -41,11 +41,11 @@ GO
 CREATE PROCEDURE sp_OnDemandCheck
 AS
 
-			/* Variáveis  -  Variables*/
-			DECLARE @DBNAME VARCHAR(100) -- Nome do banco que estará sendo verificado // Database Name that is currently been verified
-			DECLARE @LOGNAME VARCHAR(100)-- Nome lógico do arquivo de log // Logical Name for the Transaction Log File
-			DECLARE @MSG VARCHAR(100) = '' -- Retorno de conclusão com sucesso após cada base ser verificada // Sucessful message returned after each operation
-			DECLARE @SQL VARCHAR(MAX) = '' -- Construção dinâmica do comando para adequação dos VLFs // Dynamic query for VLFs configuration
+			/* VariÃ¡veis  -  Variables*/
+			DECLARE @DBNAME VARCHAR(100) -- Nome do banco que estarÃ¡ sendo verificado // Database Name that is currently been verified
+			DECLARE @LOGNAME VARCHAR(100)-- Nome lÃ³gico do arquivo de log // Logical Name for the Transaction Log File
+			DECLARE @MSG VARCHAR(100) = '' -- Retorno de conclusÃ£o com sucesso apÃ³s cada base ser verificada // Sucessful message returned after each operation
+			DECLARE @SQL VARCHAR(MAX) = '' -- ConstruÃ§Ã£o dinÃ¢mica do comando para adequaÃ§Ã£o dos VLFs // Dynamic query for VLFs configuration
 			DECLARE @CV_SIZE_MB INT = 0 -- Tamanho do arquivo de t-log na tabela ControlaVLF // Size of Transaction Log File in ControlaVLF table
 			DECLARE @FILE_SIZE_MB INT = 0 -- Tamanho do arquivo de t-log no disco // Size of Transaction Log File in disk.
 			DECLARE @COUNT SMALLINT = 1 -- Contador para o loop nas bases de dados // Counter for loop in databases
@@ -62,12 +62,12 @@ AS
 							--						ELSE 0
 							--				 END,
 
-							-- Verifica a existência de backup full para o banco de dados e atribui o valor à variável @HasFullBackup
+							-- Verifica a existÃªncia de backup full para o banco de dados e atribui o valor Ã  variÃ¡vel @HasFullBackup
 							-- Checks if the database has full backup and set the @HasFullBackup variable
 							@HasFullBackup = CASE WHEN EXISTS (SELECT TOP 1 * FROM msdb..backupset bs WHERE bs.database_name = cv.DatabaseName AND bs.type = 'D') THEN 1
 													ELSE 0
 											 END,
-							-- Monta a query dinamica com base no modelo de recuperação do banco | Substitutua o texto abaixo entre comentários /* */ pela sua rotina de backup de log
+							-- Monta a query dinamica com base no modelo de recuperaÃ§Ã£o do banco | Substitutua o texto abaixo entre comentÃ¡rios /* */ pela sua rotina de backup de log
 							-- Creates the dynamic query based on recovery model of the database | Replace the comments /* */ by your T-Log procedure
 							@SQL =	CASE WHEN RecoveryModelDesc = 'SIMPLE' AND @HasFullBackup = 1 THEN 'USE ' +@DBNAME+ '; DBCC SHRINKFILE(2,'+CAST(@CV_SIZE_MB AS VARCHAR(10))+'); USE ' +@DBNAME+ '; DBCC SHRINKFILE(2,'+CAST(@CV_SIZE_MB AS VARCHAR(10))+');'
 										ELSE 'USE MASTER; /* BACKUP LOG ' +@DBNAME+' TO DISK = ''NUL''; REPLACE THIS COMMENT BY YOUR BACKUP PROCEDURE */ USE ' +@DBNAME+ '; DBCC SHRINKFILE(2,'+CAST(@CV_SIZE_MB AS VARCHAR(10))+'); USE master; /* BACKUP LOG ' +@DBNAME+' TO DISK = ''NUL''; REPLACE THIS COMMENT BY YOUR BACKUP PROCEDURE */ USE ' +@DBNAME+ '; DBCC SHRINKFILE(2,'+CAST(@CV_SIZE_MB AS VARCHAR(10))+'); '
@@ -86,7 +86,7 @@ AS
 							AND D.is_read_only = 0
 							AND D.user_access = 0
 		
-					-- Caso o tamanho do arquivo em disco seja maior que o ideal, é realizado o shrink do arquivo, e atualizados os campos "LastShrinkDate" e "ShrinkCounter" na tabela Controla VLF
+					-- Caso o tamanho do arquivo em disco seja maior que o ideal, Ã© realizado o shrink do arquivo, e atualizados os campos "LastShrinkDate" e "ShrinkCounter" na tabela Controla VLF
 					-- If the file size in disc is bigger than the recommended, a SHRINKFILE is executed and the table ControlaVLF is updated ("LastShrinkDate" and "ShrinkCounter")
 					IF (@FILE_SIZE_MB > (@CV_SIZE_MB + 256))
 						BEGIN
@@ -98,7 +98,7 @@ AS
 							FROM master..ControlaVLF c
 							WHERE ID = @COUNT
 
-							SET @MSG = 'REALIZADO SHRINK NO BANCO: '+@DBNAME+'. ESTE PROCEDIMENTO FOI EXECUTADO PELA '+CAST(@SHRINKCOUNTER AS VARCHAR(10))+' ª VEZ !'
+							SET @MSG = 'REALIZADO SHRINK NO BANCO: '+@DBNAME+'. ESTE PROCEDIMENTO FOI EXECUTADO PELA '+CAST(@SHRINKCOUNTER AS VARCHAR(10))+' Âª VEZ !'
 							RAISERROR (@MSG,0,1) WITH NOWAIT
 				
 				
