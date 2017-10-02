2d5d79fb594bdd1a447ab3d9be96416b21360238
Edvaldo Castro <edvaldo.castro@outlook.com>
2017-10-02 20:21:42 +0100

    Rename 01-AutomaticVLFControl-Setup3.txt to 01-AutomaticVLFControl-Setup3.sql

--- 01-AutomaticVLFControl-Setup3.sql
+++ 01-AutomaticVLFControl-Setup3.sql
@@ -1,12 +1,12 @@
 /*
-	Nome: Controle Automático de VLFs  // Automatic VLF Control
+	Nome: Controle AutomÃ¡tico de VLFs  // Automatic VLF Control
 	Autor: Edvaldo Castro
 
 	Data: 05/01/2014  // Date: 2014/01/05
 	
-	OBS.: Este script funciona para as versões 2012 e 2014 do SQL Server. Nas versões anteriores, não existe o campo 
-	RecoveryUnitID. A partir da versão 2014 o Algorítimo de criação de VLFs foi alterado, de acordo com o post de 
-	Paul Randal (), porém o script continua funcionando, mas criando apenas 1 VLF, caso o tamanho definido do crescimento
+	OBS.: Este script funciona para as versÃµes 2012 e 2014 do SQL Server. Nas versÃµes anteriores, nÃ£o existe o campo 
+	RecoveryUnitID. A partir da versÃ£o 2014 o AlgorÃ­timo de criaÃ§Ã£o de VLFs foi alterado, de acordo com o post de 
+	Paul Randal (), porÃ©m o script continua funcionando, mas criando apenas 1 VLF, caso o tamanho definido do crescimento
 	do arquivo de T-log seja menor que 1/8 do tamanho deste mesmo arquivo.
 	
 	PS.: This script works for SQL Server 2012 and SQL Server 2014. In previous versions, there was not the RecoverUnitID
@@ -15,8 +15,8 @@
 	this file.
 
 	
-	Função: Controlar Dinamicamente o tamanho dos VLFs das bases de dados. Este script especificamente realiza
-			o ajuste inicial, adequando os arquivos de Transaction Log de todas as bases de dados da instância.
+	FunÃ§Ã£o: Controlar Dinamicamente o tamanho dos VLFs das bases de dados. Este script especificamente realiza
+			o ajuste inicial, adequando os arquivos de Transaction Log de todas as bases de dados da instÃ¢ncia.
 
 	Function:	Dynamically control the VLF size of all databases in a single SQL Server instance. This script do
 				executes the initial adjustments, setting up the size and VLFs for Transaction Log files.
@@ -24,11 +24,11 @@
 
 	Disclaimer:
 			
-		Os passos e scripts a seguir foram desenvolvidos para automatizar a configuração e controle dos Virtual Log Files,
-		e podem causar algum tipo de bloqueio ou indisponibilidade temporária do SQL Server. 
-		Antes de implantar ou utilizar partes ou todo o conteúdo aqui disponibilizado, sugiro a leitura e entendimento do que 
-		cada passo realiza. Implante primeiro em um ambiente de testes controlado, e caso veja benefícios, se você for executá-los 
-		em seu ambiente de produção, por sua conta e risco.
+		Os passos e scripts a seguir foram desenvolvidos para automatizar a configuraÃ§Ã£o e controle dos Virtual Log Files,
+		e podem causar algum tipo de bloqueio ou indisponibilidade temporÃ¡ria do SQL Server. 
+		Antes de implantar ou utilizar partes ou todo o conteÃºdo aqui disponibilizado, sugiro a leitura e entendimento do que 
+		cada passo realiza. Implante primeiro em um ambiente de testes controlado, e caso veja benefÃ­cios, se vocÃª for executÃ¡-los 
+		em seu ambiente de produÃ§Ã£o, por sua conta e risco.
 
 	Disclaimer
 
@@ -70,7 +70,7 @@ AS
 			  FileState VARCHAR(10)
 			)
 
-		--CRIA TABELA TEMPORÁRIA PARA COLETAR INFORMAÇÕES DO DBCC LOGINFO
+		--CRIA TABELA TEMPORÃRIA PARA COLETAR INFORMAÃ‡Ã•ES DO DBCC LOGINFO
 		IF OBJECT_ID('tempdb..##LogInfo') IS NOT NULL
 			DROP TABLE ##LogInfo
 		GO
@@ -89,7 +89,7 @@ AS
 			  CreateLSN VARCHAR(40)
 			)
 
-		--COLETA INFORMAÇÕES DOS VLFs
+		--COLETA INFORMAÃ‡Ã•ES DOS VLFs
 		EXEC sp_msforeachdb '
 		use [?];
 		IF DB_ID() > 4
@@ -100,7 +100,7 @@ AS
 
 		'
 
-		--ARMAZENA AS INFORMAÇOES RELEVANTES REFERENTES AOS VLFs
+		--ARMAZENA AS INFORMAÃ‡OES RELEVANTES REFERENTES AOS VLFs
 		USE master
 		GO
 		INSERT  INTO ControlaVLF
