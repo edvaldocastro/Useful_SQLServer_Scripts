5ee8cd5495f9b8586ef504e472bc3c5278cabd6d
Edvaldo Castro <castroe@ryanair.com>
2023-07-04 08:47:18 +0100

    modified file ShrinkFile.sql

--- TSQLAdmin/ShrinkFile.sql
+++ TSQLAdmin/ShrinkFile.sql
@@ -1,10 +1,9 @@
-
 --https://www.sqlshack.com/shrinking-your-database-using-dbcc-shrinkfile/
  
-DECLARE @FileName sysname = N'MSDBData';
+DECLARE @FileName sysname = N'tempdev';
 DECLARE @TargetSize INT = (SELECT 1 + size*8./1024 FROM sys.database_files WHERE name = @FileName);
---DECLARE @Factor FLOAT = .999;
-DECLARE @Factor FLOAT = .019;   -- 1/dbsize will give you a load factor that shrinks around 1GB per loop
+DECLARE @Factor FLOAT = .999;
+--DECLARE @Factor FLOAT = .019;   -- 1/dbsize will give you a load factor that shrinks around 1GB per loop
  
 WHILE @TargetSize > 0
 BEGIN
@@ -12,6 +11,6 @@ BEGIN
     DBCC SHRINKFILE(@FileName, @TargetSize);
     DECLARE @msg VARCHAR(200) = CONCAT('Shrink file completed. Target Size: ', 
          @TargetSize, ' MB. Timestamp: ', CURRENT_TIMESTAMP);
-    RAISERROR(@msg, 1, 1) WITH NOWAIT;
-    WAITFOR DELAY '00:00:01';
+    RAISERROR(@msg, 1, 1) WITH NOWAIT
+    WAITFOR DELAY '00:00:01'
 END;
